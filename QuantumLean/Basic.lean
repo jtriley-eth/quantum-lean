5e0a8d58a31238d5c668620fc8fbd29ef53e0637
jtriley.eth <Jtriley15@gmail.com>
2024-09-05 12:19:54 -0700

    init commit

---
+++ QuantumLean/Basic.lean
@@ -0,0 +1,10 @@
+import Mathlib.Data.Matrix.Basic
+import Mathlib.Data.Complex.Basic
+
+open Complex
+
+def realOnly : ℂ := 1
+def imaginaryOnly : ℂ := I
+
+def both : ℂ := 1 + I
+def alsoBoth : ℂ := ⟨1, 1⟩
